4ae988ac87fd333afd7ee0a476dac22e8c04b58c
Madhura <madhura.bhujbal@gmail.com>
2021-03-31 15:09:42 +0100

    Add code to fetch reddit feeds and store them into an array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState('webdev');
+
+  useEffect(() => {
+    fetch("https://www.reddit.com/r/webdev.json").then(res => {
+      if(res.status != 200) {
+        console.log("ERROR");
+        return;
+      }
+
+      res.json().then(data => {
+        if (data != null) {
+          console.log(data);
+          setArticles(data.data.children);
+        }
+      });
+    })
+  }, [subreddit]);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        Header
+        <input type="text" className="input" value="webdev"></input>
       </header>
+      <div className="articles"></div>
     </div>
   );
 }
