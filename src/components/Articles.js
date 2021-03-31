816a032b697c01bc56dbc3b4c31461d87a4520f6
Madhura <madhura.bhujbal@gmail.com>
2021-03-31 15:21:50 +0100

    Create a new article component

---
+++ src/components/Articles.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Articles() {
+    return (
+        <article>
+            <a href='#'>
+                <h3>
+                    Title
+                </h3>
+            </a>
+        </article>
+    )
+}
+
+export default Articles;
